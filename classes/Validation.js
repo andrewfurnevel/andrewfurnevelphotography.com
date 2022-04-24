89f2451a5ce8e174db910cf2e29a15b33c725463
Andrew Furnevel <andyfurnevel@gmail.com>
2022-04-24 14:34:06 -0700

    Added the check for additional arguments to the for loops and created a test that console.logs to the screen the name of the method that is to be called. Works but still a work in progress!

--- classes/Validation.js
+++ classes/Validation.js
@@ -13,16 +13,16 @@ class Validation {
         this.rules = [];
     }
 
-    validate(formObj,valRules) {
-        console.log(formObj, valRules);
-        for (const input in formObj) {
-            console.log(`${input} : ${formObj[input]}`);
-            for (const rule in valRules) {
-                console.log(`${rule} : ${valRules[rule]}`)
-            }
-        }
+    // validate(formObj,valRules) {
+    //     console.log(formObj, valRules);
+    //     for (const input in formObj) {
+    //         console.log(`${input} : ${formObj[input]}`);
+    //         for (const rule in valRules) {
+    //             console.log(`${rule} : ${valRules[rule]}`)
+    //         }
+    //     }
 
-    }
+    // }
 
 
     setRules(rules) {
@@ -56,50 +56,45 @@ class Validation {
         // console.log(this.rules[0][3].length);
 
         for ( let i = 0; i < this.rules.length; i++) {
-            console.log(this.rules[i][2]);
 
+            // Get User Input from form (req.body)
+            let userInput = this.rules[i][2];
+            console.log(userInput);
+
+            
+            
             for ( let j = 0; j < this.rules[i][3].length; j++) {
                 console.log(this.rules[i][3][j]);
-            }
+
+                // Set currentRule
+                let currentRule = this.rules[i][3][j];
                 
-                
+                const containsArg = this.checkForArgs(currentRule);
+
+                if (containsArg) {
+                    console.log('Yes it has additional args!!!');
+                    const newArr = currentRule.slice(0, -1).split('[');
+                    console.table(newArr);
+                }  else {
+                    console.log(`Calling the ${currentRule} Method`);
+                }  
+            }             
         } 
 
-        let currentRule = 'minLength[5]';
-
-        const containsArg = this.checkForArgs(currentRule);
-    
-        if (containsArg) {
-            console.log('Yes it has additional args!!!');
-            const newArr = currentRule.slice(0, -1).split('[');
-            console.table(newArr);
-        }    
-
         // console.table(this.rules);
-        
         // console.log(this.rules[0][3][0]);
         // console.log(this.rules[3][2].length);
 
-    
-        // this.rules[0][3].forEach()
-
-
-
-
-
-        let theString = this.rules[0][3][0];
-
-        // console.log(theString);
 
         // NOTE: I'm using eval() which is considered a bad idea and
         // should be replaced with a safe alternative!!! But it works!!!!!
+        
+        
+        let theString = this.rules[0][3][0];
+        console.log(theString);
         const validationMethod = String(theString);
-
         eval(`this.${validationMethod}()`);
-
-        // this.hasNumbers("kjhjl");
     }
-
 
 
     testRun() {
@@ -124,6 +119,7 @@ class Validation {
         if (val) {
             validationErrors.push();
         }
+        console.log('regexMatch Method Called');
     }
 
     differs(val_a, val_b) {}
@@ -132,140 +128,166 @@ class Validation {
         if (!val) {
             validationErrors.push();
         }
+        console.log('isUnique Method Called');
     }
 
     minLength(val, min) {
         if (val.length < min) {
             validationErrors.push(`Input Must be A Minimum of ${min} Characters`); 
         }
+        console.log('minLength Method Called');
     }
     maxLength(val, max) {
         if (val.length > max) {
             validationErrors.push('Input Exceeds the Maximun Length!');
         }
+        console.log('maxLength Method Called');
     }
 
     exact_length(val, len) {
         if (val.length !== len) {
             validationErrors.push();
         }
+        console.log('exactLength Method Called');
     }
 
     greater_than(val) {
         if (val) {
             validationErrors.push();
         }
+        console.log('greaterThan Method Called');
     }
+
     great_than_equal_to(val) {
         if (val) {
             validationErrors.push();
         }
+        console.log('greaThanEqualTo Method Called');
     }
 
     less_than(val) {
         if (val) {
             validationErrors.push();
         }
+        console.log('lessThan Method Called');
     }
 
     less_than_equal_to(val) {
         if (val) {
             validationErrors.push();
         }
+        console.log('lessThanEqualTo Method Called');
     }
 
     in_list(val, arr){
         if (val) {
             validationErrors.push();
         }
+        console.log('inList Method Called');
     }
 
     alpha(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('alpha Method Called');
     }
 
     alpha_numeric(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('alphaNumeric Method Called');
     }
 
     alpha_numeric_spaces(val) {
         if (val) {
             validationErrors.push();
         }
+        console.log('alphaNUmericSpaces Method Called');
     }
 
     alpha_dash(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('alphaDash Method Called');
     }
 
     numeric(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('numeric Method Called');
     }
 
     integer(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('integer Method Called');
     }
 
     decimal(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('decimal Method Called');
     }
 
     is_natural(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('isNatural Method Called');
     }
 
     is_natural_no_zero(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('isNaturalNoZero Method Called');
     }
 
     valid_url(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('validURL Method Called');
     }
 
     valid_email(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('validEmail Method Called');
     }
 
     valid_emails(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('validEmails Method Called');
     }
 
     valid_ip(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('validIP Method Called');
     }
 
     valid_base64(val){
         if (val) {
             validationErrors.push();
         }
+        console.log('validBase64 Method Called');
+    }
+
+    allowableChars(val) {
+        console.log('allowableChars Method Called');
     }
 
 } // End Class
-
-// const val = new Validation;
 
 module.exports = { Validation }
