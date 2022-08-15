f0851eb0e73abda170acc8ffc40f4e517cabc8c1
Andy Furnevel <github@spongecreative.com>
2022-08-15 16:21:10 -0700

    Updated .gitignore

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 app.use("/", publicRoutes);
 
 // 404 Page Not Found
